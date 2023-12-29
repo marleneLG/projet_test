3a38460973974907e795c2de539867cc1a7a22dc
marlene <marlene.mlg53@gmail.com>
2023-12-29 17:06:35 +0100

    add homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+
+    #[Route('/home', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
