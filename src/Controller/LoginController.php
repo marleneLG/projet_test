81a998fd22e664dd4957baf254726a5294d859d8
marlene <marlene.mlg53@gmail.com>
2024-01-06 16:15:15 +0100

    feature logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class LoginController extends AbstractController
 {
@@ -25,4 +26,18 @@ class LoginController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('login/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    #[Route('/logout', name: '_logout_main')]
+    public function logout(Security $security): Response
+    {
+        dump('coucou');
+        // logout the user in on the current firewall
+        $response = $security->logout();
+
+        // you can also disable the csrf logout
+        $response = $security->logout(false);
+        return $response;
+        // ... return $response (if set) or e.g. redirect to the homepage
+    }
+
 }
